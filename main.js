1e469864426e2d576bb178435e4b4e3eeca0f1de
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-09-06 09:35:45 +0100

    Added another way to spend cookies

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ function cookieClick(number) {
 var grannies = 0;
 
 function buyGranny() { 
-  var grannyCost = Math.floor(10 * Math.pow(1.1,grannies));
-    if(cookies >= grannyCost) {
-      grannies = grannies + 1;
-      cookies = cookies - grannyCost;
-      document.getElementById('grannies').innerHTML = grannies;
-      document.getElementById('cookies').innerHTML = cookies;
+  var grannyCost = Math.floor(10 * Math.pow(1.1,grannies)); //works out the cost of the item
+    if(cookies >= grannyCost) { //checks that the player can afford the upgrade
+      grannies = grannies + 1; //adds one cursor
+      cookies = cookies - grannyCost; //deducts the cost of item from your total
+      document.getElementById('grannies').innerHTML = grannies; //updates the number of items shown to you
+      document.getElementById('cookies').innerHTML = cookies; //updates your cookie total
     };
-  var nextCost = Math.floor(10 * Math.pow(1.1,grannies));
-  document.getElementById('grannyCost').innerHTML = nextCost;
+  var nextCost = Math.floor(10 * Math.pow(1.1,grannies)); //calculation that works out the cost of the next item
+  document.getElementById('grannyCost').innerHTML = nextCost; //updates the cost of the item, as displayed to the user.
   };
 
   var machines = 0;
@@ -32,8 +32,6 @@ function buyMachines() {
   var nextCost = Math.floor(100 * Math.pow(1.1,machines));
   document.getElementById('machineCost').innerHTML = nextCost;
   };
-
-  cookieClick(machines);
 
 
 window.setInterval(function(){
